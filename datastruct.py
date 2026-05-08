95fb1fafc7e8d171ca06f650e22e35e2d54633e5
JS Ng <ngjunsiang@gmail.com>
2026-05-08 03:51:42 +0000

    sample implementation

--- datastruct.py
+++ datastruct.py
@@ -24,8 +24,9 @@ class Node:
     """
 
     def __init__(self, data: tuple[int, int]):
-        # Replace the line below with your code
-        raise NotImplementedError
+        # store data and next pointer
+        self._data = data
+        self.next: "Node | None" = None
 
     def __repr__(self) -> str:
         return f'Node({self.get()})'
@@ -39,8 +40,7 @@ class Node:
         Return
             tuple[int, int]
         """
-        # Replace the line below with your code
-        raise NotImplementedError
+        return self._data
 
 
 class LinkedList:
@@ -50,7 +50,8 @@ class LinkedList:
         None
 
     Attributes
-        None
+        _head: Node object if linked list is not empty
+               None if linked list is empty
 
     Methods
         - length() -> int
@@ -75,8 +76,12 @@ class LinkedList:
         Return
             length of linkedlist as an integer (zero or positive)
         """
-        # Replace the line below with your code
-        raise NotImplementedError
+        count = 0
+        current = self._head
+        while current is not None:
+            current = current.next
+            count += 1
+        return count
 
     def get(self, n: int) -> tuple[int, int]:
         """Returns item at n-th node.
@@ -91,8 +96,25 @@ class LinkedList:
         Raises
             IndexError if n >= length
         """
-        # Replace the line below with your code
-        raise NotImplementedError
+        # Validation: ensure index is 0 or positive
+        if n < 0:
+            raise IndexError("n must be 0 or positive")
+        # Check if linkedlist is empty
+        if self._head is None:
+            raise IndexError("linkedlist is empty")
+        current = self._head
+        # Attempt to reach the nth node
+        while (
+                n > 0  # Exit if nth node reached
+                and current is not None  # Exit if passed tail
+        ):
+            current = current.next
+            n -= 1
+        # If passed the tail, raise IndexError
+        if current is None:
+            raise IndexError("n >= length")
+        # nth node is reached: return item
+        return current.get()
 
     def insert(self, n: int, item: tuple[int, int]) -> None:
         """Insert item into linkedlist at position n.
@@ -107,8 +129,27 @@ class LinkedList:
         Raises
             IndexError if n > length
         """
-        # Replace the line below with your code
-        raise NotImplementedError
+        # Validation: ensure index is 0 or positive
+        if n < 0:
+            raise IndexError("n must be 0 or positive")
+        if n == 0:
+            node = Node(item)
+            node.next = self._head
+            self._head = node
+        else:  # n > 0
+            # Get the prev node (at index n - 1)
+            prev = self._head
+            while (
+                n > 1
+                and prev is not None
+            ):
+                prev = prev.next
+                n -= 1
+            if prev is None:  # passed tail
+                raise IndexError("n > length")
+            node = Node(item)
+            node.next = prev.next
+            prev.next = node
 
     def append(self, item: tuple[int, int]) -> None:
         """Append item at the end of linkedlist.
@@ -120,8 +161,15 @@ class LinkedList:
         Returns
             None
         """
-        # Replace the line below with your code
-        raise NotImplementedError
+        # Check if linked list is empty
+        if self._head is None:
+            self._head = Node(item)
+        else:
+            # get last node
+            current = self._head
+            while current.next is not None:
+                current = current.next
+            current.next = Node(item)
 
     def delete(self, n: int) -> None:
         """Delete n-th item from linkedlist.
@@ -133,9 +181,25 @@ class LinkedList:
         Raises
             IndexError if n >= length
         """
-        # Replace the line below with your code
-        raise NotImplementedError
-       
+        # Validation: ensure index is 0 or positive
+        if n < 0:
+            raise IndexError("n must be 0 or positive")
+        if self._head is None:
+            raise IndexError("linked list is empty")
+        if n == 0:  # head deletion
+            self._head = self._head.next
+            return
+        prev = self._head
+        while (
+                n > 1
+                and prev.next is not None
+        ):
+            prev = prev.next
+            n -= 1
+        if prev.next is None:
+            raise IndexError("n >= length")
+        prev.next = prev.next.next
+
     def contains(self, item: tuple[int, int]) -> bool:
         """Checks whether an item is in the linkedlist.
         Returns a boolean value to indicate the status of the search.
@@ -148,11 +212,23 @@ class LinkedList:
             True if item is found in the linkedlist,
             otherwise False
         """
-        # Replace the line below with your code
-        raise NotImplementedError
+        # Check for empty linkedlist
+        if self._head is None:
+            return False
+        current = self._head
+        while current is not None:
+            if current.get() == item:
+                return True
+            current = current.next
+        return False
 
 
 if __name__ == "__main__":
     # Write any test code here and run it with
     # `python datastruct.py`
-    pass
+    llist = LinkedList()
+    llist.append("a")  # type: ignore
+    llist.append("b")  # type: ignore
+    print("Index 0:", llist.get(0))  # a
+    print("Index 1:", llist.get(1))  # b
+    print("Length:", llist.length())  # 2
